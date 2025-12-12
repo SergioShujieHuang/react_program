3b2d851fc0ffd262f5fdf029a5ba62dac71f1821
SergioShujieHuang <sergio.shujie.huang@gmail.com>
2025-12-12 08:44:09 +0000

    wip: ListGroup components

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,25 @@
+function ListGroup() {
+    let item = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    return (
+        <>
+            <h1>List</h1>
+            {item.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+                {item.map((item, index) => (
+                    <li className="list-group-item"
+                        key={item}
+                        onClick={() => console.log(item, index)}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
